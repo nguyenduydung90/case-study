9534f682fac2a21f1aa0a9bc6627be1ca2043314
nguyenduydung90 <dunght38@gmail.com>
2020-10-01 14:35:38 +0700

    updata

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ class Question {
 }
 
 let index = 1;
-let timeCount = 15;
+let timeCount;
 let message;
 let timeID = setInterval(function () {
-    document.getElementById('time1').innerHTML = timeCount;
+    // document.getElementById('time1').innerHTML = timeCount;
     timeCount--;
     countdown();
 }, 1000);
 
-let question1 = new Question("Câu 1:Ai sẽ học lại Modul 1?", ["A.Vũ Đức Dương", "B.Nguyễn Đức Việt", "C.Trọng chó", "D.Không có ai cả"], "D.Không có ai cả", "10xu");
-let question2 = new Question("Câu 2: CodeGym có bao nhiêu trung tâm?", ["A. 4", "B. 5", "C. 6", "D. 7"], "B. 5", "20xu");
-let question3 = new Question("Câu 3:'El Nino' là gì?", ["A. Một món ăn", "B. Một loại cây", "C. Một hiện tượng thời tiết", "D. Một địa danh"], "C. Một hiện tượng thời tiết", "30xu");
-let question4 = new Question("Câu 4:Nòng nọc xuất hiện trong vòng đời của loài vật nào?", ["A. Rắn", "B. Tằm", "C. Ếch", "D. Bướm"], "C. Ếch", "40xu");
-let question5 = new Question("Câu 5:Loại củ nào giúp vết thương mau lành, không để lại sẹo?", ["A. Gừng", "B. Hành", "C. Ớt", "D. Nghệ"], "D. Nghệ", "50xu");
+let question1 = new Question("Câu 1: Rượu vang được lên men từ loại quả nào là chủ yếu?", ["A.Táo", "B.Cam", "C.Xoài", "D.Nho"], "D.Nho", "1.000.000 VNĐ");
+let question2 = new Question("Câu 2: CodeGym có bao nhiêu trung tâm?", ["A. 4", "B. 5", "C. 6", "D. 7"], "B. 5", "1.500.000 VNĐ");
+let question3 = new Question("Câu 3:'El Nino' là gì?", ["A. Một món ăn", "B. Một loại cây", "C. Một hiện tượng thời tiết", "D. Một địa danh"], "C. Một hiện tượng thời tiết", "2.000.000 VNĐ");
+let question4 = new Question("Câu 4:Nòng nọc xuất hiện trong vòng đời của loài vật nào?", ["A. Rắn", "B. Tằm", "C. Ếch", "D. Bướm"], "C. Ếch", "2.500.000 VNĐ");
+let question5 = new Question("Câu 5:Loại củ nào giúp vết thương mau lành, không để lại sẹo?", ["A. Gừng", "B. Hành", "C. Ớt", "D. Nghệ"], "D. Nghệ", "5.000.000 VNĐ");
 
 let question = [question1, question2, question3, question4, question5];
 let getQuestion = document.getElementById('question');
@@ -35,6 +35,7 @@ showQuestion(question1);
 function next(index) {
     ++index;
     showQuestion(question[index]);
+
 
 }
 
@@ -49,25 +50,28 @@ function showQuestion(question_1) {
 }
 
 function chooseAswer(id) {
-
-    if (!confirm("Đây có phải là đáp án cuối cùng của bạn không")) {
-
-        return ;
-    }
-
     a.innerHTML = soundCorect.getSound()
     let answer = document.getElementById(id).innerHTML;
     let getQestId1 = document.getElementById('question');
     let idQuestion = parseInt(getQestId1.getAttribute("idQuestion"));
 
+    if (!confirm("Đây có phải là đáp án cuối cùng của bạn không")) {
 
+        return ;
+    }
     if (question[idQuestion].checkAnswer(answer)) {
-
         alert('Chúc mừng bạn. Câu trả lời đúng');
-        document.getElementById('result').innerHTML = "Tiền thưởng: "+(question[idQuestion].money);
-        next(idQuestion);
+        document.getElementById('result').innerHTML = "Tiền thưởng: " + (question[idQuestion].money);
 
-    } else {
+        if (idQuestion === 4) {
+            a.innerHTML = win.getSound()
+            alert("You win");
+            timeCount=1
+            // window.reload();
+        }
+        next(idQuestion);
+    }
+     else {
         a.innerHTML = soundWrong.getSound();
         alert('Câu trả lời sai. Mời bạn chơi lại');
         timeCount=1;
@@ -78,13 +82,8 @@ function chooseAswer(id) {
 function countdown() {
     if (timeCount < -1) {
         clearInterval(timeID);
-        message = window.confirm('Chơi lại');
-        //
+        message = window.confirm('Mời bạn chơi lại');
         reload()
-        // if (message) {
-        //     //
-        //     reload()
-        // }
     }
 }
 
--- suond.js
+++ suond.js
@@ -24,8 +24,13 @@ let souND = "<audio autoplay>" +
     '<source src="sound/sai.ogg" type="audio/mpeg">' +
     '</audio>';
 
+let winSound= "<audio autoplay>" +
+    '<source src="sound/C15_dung.ogg" type="audio/mpeg">' +
+    '</audio>';
+
 let sound1 = new Sound(sound);
 let soundCorect = new Sound(sounD);
 let soundWrong = new Sound(souND);
+let win= new Sound(winSound)
 let a = document.getElementById('sound')
 a.innerHTML = sound1.getSound();
